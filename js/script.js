95cc7e296407877b4a5eab9833d367eae1dd87ca
Michał Kuliś <michalkulis1993@gmail.com>
2018-06-03 22:22:29 +0200

    Update script.js file

--- js/script.js
+++ js/script.js
@@ -1,38 +1,36 @@
 'use strict';
 (function () {
+    
+    var modalLinks = document.querySelectorAll('.show-modal');
+    var closeButtons = document.querySelectorAll('.modal .close');
+    var modals = document.querySelectorAll('.modal');
 
     var showModal = function (event) {
         event.preventDefault();
         document.querySelector('#modal-overlay').classList.add('show');
-        document.querySelector(event.target.attributes.href.nodeValue).classList.add('show');
+        document.querySelector(event.target.attributes.href.value).classList.add('show');
     };
 
-    var modalLinks = document.querySelectorAll('.show-modal');
-
-    for (var i = 0; i < modalLinks.length; i++) {
-        modalLinks[i].addEventListener('click', showModal);
-    }
+    modalLinks.forEach(function (item) {
+        item.addEventListener('click', showModal);
+    });
 
     var hideModal = function (event) {
         event.preventDefault();
         document.querySelector('#modal-overlay').classList.remove('show');
     };
 
-    var closeButtons = document.querySelectorAll('.modal .close');
-
-    for (var i = 0; i < closeButtons.length; i++) {
-        closeButtons[i].addEventListener('click', hideModal);
-    }
+    closeButtons.forEach(function(item) {
+       item.addEventListener('click',hideModal); 
+    });
 
     document.querySelector('#modal-overlay').addEventListener('click', hideModal);
 
-    var modals = document.querySelectorAll('.modal');
-
-    for (var i = 0; i < modals.length; i++) {
-        modals[i].addEventListener('click', function (event) {
+    modals.forEach(function(item) {
+        item.addEventListener('click',function(event) {
             event.stopPropagation();
             modals.classList.remove('show');
             showModal();
-        });
-    }
+        })
+    });
 })();
